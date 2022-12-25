13ba26d42b3f2b1b82fb03c02c0f13b3c5d19ea2
Takuya Ikeshima <ikeshi1222@gmail.com>
2022-12-25 09:51:10 +0000

    Laravel04課題提出

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(NewsController::class)->prefix("admin")->group(function(){
+    Route::get("news/create", "add");
+});
+
+
+// 3. 「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+Route::controller(AAAController::class)->group(function(){
+    Route::get('XXX','bbb');
+});
+
+
+// 4. 【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+//     web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら 
+//     ProfileController の edit Action に割り当てるように設定してください
+
+Route::controller(ProfileController::class)->prefix("admin")->group(function(){
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
